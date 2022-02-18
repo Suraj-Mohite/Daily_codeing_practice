dd80b68185c5d02b46b230cca94bb03521b40c15
Suraj <surajmohite1997@gmail.com>
2022-02-18 23:58:00 +0530

    BS Question

--- Daily_Code/Binary_Search/q2_numOfTimeArrayRotated.py
+++ Daily_Code/Binary_Search/q2_numOfTimeArrayRotated.py
@@ -11,7 +11,7 @@ def numOfTimeRotated(arr):
             end=mid
         else:
             start=mid+1
-    return start
+    return arr[start]
 
 # print(numOfTimeRotated([4,3]))
 
@@ -25,13 +25,15 @@ def numOfTimeRotatedDuplicate(arr):
     while start<end:
         mid=start+(end-start)//2
 
-        if arr[mid]<=arr[end]:
+        if arr[mid]<arr[end]:
             end=mid
-        else:
+        elif arr[mid]>arr[end]:
             start=mid+1
-    return start
+        else:
+            end-=1
+    return arr[start]
 
-# print(numOfTimeRotatedDuplicate([7,7,7,4,4,5,5,5,6]))
+print(numOfTimeRotatedDuplicate([1,1,1,0,0,1]))
 
 
 
@@ -51,7 +53,27 @@ def maxInRotatedSortedArray(arr):
         else:
             end=mid-1
     if start<len(arr)-1 and arr[start+1]>arr[start]:
-        return start+1
-    return start
+        return arr[start+1]
+    return arr[start]
 
-print(maxInRotatedSortedArray([8,9,10,11,13,6]))
+# print(maxInRotatedSortedArray([8,9,10,11,12,13]))
+
+# #find max in rotated sorted array  Duplicate
+
+def maxInRotatedSortedArray(arr):
+    start=0
+    end=len(arr)-1
+
+    while start<end:
+        mid=start+(end-start)//2
+
+        if mid!=start and arr[mid]>=arr[start]:
+            start=mid
+        else:
+            end=mid-1
+    if start<len(arr)-1 and arr[start+1]>=arr[start]:
+        return arr[start+1]
+    return arr[start]
+
+# print(maxInRotatedSortedArray([1,0,1,1,1,1]))
+# print(maxInRotatedSortedArray([5,5,5,5,5,5]))
---
+++ Daily_Code/Binary_Search/q6_searchinRotatedsortArr.py
@@ -0,0 +1,6 @@
+def searchInRotatedSortedArr(arr,target):
+    start=0
+    end=len(arr)-1
+
+    while start<end:
+        pass
